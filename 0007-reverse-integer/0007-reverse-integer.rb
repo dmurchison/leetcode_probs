8a30f393fcadaab6fb348f7900975062362b361d
Duncan Murchison <duncancmurchison@gmail.com>
2022-10-21 00:18:08 -0400

    Time: 148 ms (51.13%), Space: 210.9 MB (98.19%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.rb
@@ -0,0 +1,8 @@
+# @param {Integer} x
+# @return {Integer}
+def reverse(x)
+    neg = x < 0
+    x = x.to_s.reverse.to_i
+    return  0 if (x > 2147483646 || x < -2147483647)
+    return neg ? -x : x
+end
