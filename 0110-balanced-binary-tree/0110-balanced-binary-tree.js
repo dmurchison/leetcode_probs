70d8cc985374bfdf2c4ebe8f3463dafa35b7a045
Duncan Murchison <duncancmurchison@gmail.com>
2022-10-31 16:11:55 -0400

    Time: 127 ms (47.15%), Space: 46.9 MB (82.23%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+const ckeckHeight = node => {
+    if (node === null) return 0;
+    const left = ckeckHeight(node.left);
+    const right = ckeckHeight(node.right);
+    if (
+        left === false ||
+        right === false ||
+        Math.abs(left - right) > 1
+    ) {
+        return false;
+    }
+    return Math.max(left, right) + 1;
+};
+
+var isBalanced = function(root) {
+    if (root === null) return true;
+  
+    return ckeckHeight(root) !== false;
+};
