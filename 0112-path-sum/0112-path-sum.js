4a7ca7cd0e5918ed107538a74a3b96230156aacb
Duncan Murchison <duncancmurchison@gmail.com>
2022-11-01 10:59:27 -0400

    Time: 111 ms (63.32%), Space: 45.4 MB (80.60%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    return checkTree(root, targetSum, 0);
+    function checkTree(root, targetSum, sum) {
+        if (!root) {
+            return false;
+        }
+        sum += root.val;
+        if (root.left === null && root.right === null) {
+            return sum === targetSum;
+        }
+        return checkTree(root.left, targetSum, sum) || checkTree(root.right, targetSum, sum)
+    };
+
+};
